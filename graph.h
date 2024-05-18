798d7da6d5d47ac77f321dd70b7a7653f00ce9d2
Ezhil Dhiraviya <115528036+EzhilDhiraviya@users.noreply.github.com>
2024-05-18 13:35:51 +0530

    graph.h added

---
+++ graph.h
@@ -0,0 +1,77 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+map<int,string> Map;
+void mapping(string file_name){
+
+    ifstream file(file_name);
+    int i;
+    string s;
+    while(file>>i>>s){
+        Map[i] = s;
+    }
+    
+}
+class Graph
+{
+    int no_of_vertices;
+    struct vertex
+    {
+        int vertex_no;
+    };
+    struct edge
+    {
+        int weight;
+        struct vertex *from;
+        struct vertex *to;
+    };
+
+    vector<vertex> vertices;
+    vector<edge> edges;
+
+    public:
+
+    Graph(int n)
+    {
+        no_of_vertices = n;
+        vertices.resize(n);
+        for(int i=0;i<n;i++)
+        {
+            vertices[i].vertex_no = i;
+        }
+        mapping("mapping.txt");
+    }
+
+    void addEdge(int from, int to, int weight)
+    {
+        edge e;
+        e.weight = weight;
+        e.from = &vertices[from];
+        e.to = &vertices[to];
+        edges.push_back(e);
+        e.from = &vertices[to];
+        e.to = &vertices[from];
+        edges.push_back(e);
+    }
+
+    void displayAdjacencyList()
+    {
+        vector<vector<int>> adjacencyList(no_of_vertices);
+        for(int i=0;i<edges.size();i++)
+        {
+            adjacencyList[edges[i].from->vertex_no].push_back(edges[i].to->vertex_no);
+        }
+        for(int i=0;i<no_of_vertices;i++)
+        {
+            cout<<Map[i]<<" -> ";
+            for(int j=0;j<adjacencyList[i].size();j++)
+            {
+                cout<<Map[adjacencyList[i][j]]<<" ";
+            }
+            cout<<endl;
+        }
+    }
+    
+};
+
+
