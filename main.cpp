f27e19898a3d691d1ce287006e79c5ffbc674547
jothiruban <jothiruban1512@gmail.com>
2024-05-18 15:55:05 +0530

    added avl storage

--- main.cpp
+++ main.cpp
@@ -1,23 +1,52 @@
 #include <bits/stdc++.h>
 using namespace std;
+#include "avl.h"
 
 map<int,string> Map;
+map<string,string>SyllabusMap;
 
-void mapping(string file_name){
+void mapping(){
 
-    ifstream file(file_name);
+    ifstream fileformap("mapping.txt");
+    ifstream fileforSyllabus("syllabus.txt");
     int i;
     string s;
-    while(file>>i>>s){
+    while(fileformap>>i>>s){
         Map[i] = s;
     }
-    
+    string id,syll;
+    while(fileforSyllabus>>id>>syll){
+        SyllabusMap[id] = syll;
+    }
+}
+
+void printMap(){
+    for(int i=0;i<17;i++){
+        cout<<i+1<<"-->"<<Map[i]<<"\n";
+    }
+}
+
+void printSyllabus(){
+    for(auto i: SyllabusMap){
+        cout<<i.first<<": "<<i.second<<"\n";
+    }
+}
+
+void storeSyllabus(AvlTree<string>t){
+
+    for(auto i: SyllabusMap){
+        t.insert(i.first);
+    }
+
+    t.printTreeStructure();
 }
 
 
-
 int main(){
-    mapping("mapping.txt");
+    AvlTree<string>t;
 
+    mapping();
+    printSyllabus();
+    storeSyllabus(t);
     return 0;
 }
